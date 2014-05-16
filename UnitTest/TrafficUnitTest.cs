cfbfa5072f111ddd54662ee73aa3d73e11f41635
alexan1 <alexan1@gmail.com>
2014-05-16 15:15:49 -0400

    3

--- UnitTest/TrafficUnitTest.cs
+++ UnitTest/TrafficUnitTest.cs
@@ -8,27 +8,27 @@ namespace UnitTest
     public class TrafficUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetTimeToWait_WithFiveMinutes_ShouldReturnZero()
         {
             var light = new Light
             {
                 SwitchTime = 10
             };
 
-            var actual = light.TimeToWait(5);
+            var actual = light.GetTimeToWait(5);
             var expected = 0;
             Assert.AreEqual(expected, actual, "Should be zero");
         }
 
         [TestMethod]
-        public void TestMethod2()
+        public void GetTimeToWait_WithFifteenMinutes_ShouldReturnFive()
         {
             var light = new Light
             {
                 SwitchTime = 10
             };
 
-            var actual = light.TimeToWait(15);
+            var actual = light.GetTimeToWait(15);
             var expected = 5;
             Assert.AreEqual(expected, actual, "Should be 5");
         }
