c350bfb4cafdc8c9dfce5a30472db8f8b53589a6
alexan1 <alexan1@gmail.com>
2014-05-20 12:25:10 -0400

    fsharp

--- SalexTaxes_F/Product.fs
+++ SalexTaxes_F/Product.fs
@@ -3,38 +3,51 @@
 module Product = 
 
     type ProductType = 
-        |food = 1
-        |book = 2
-        |medical = 3
-        |other = 4
+        |Food
+        |Book
+        |Medical
+        |Other
 
     type Product =
         {Name : string;
         Type : ProductType;        
         Price : decimal;        
-        IsImport: bool;
-        IsExempt : bool}
-    
-    //type Taxes = Product.Price
-    let Tax rate price = (price : decimal) * rate
-    let BasicTax = Tax 0.1m
-    let ImportDuty = Tax 0.05m 
+        IsImport: bool}
+            
+    let tax rate price = price * rate
+    let basicTax = tax 0.1m
+    let importDuty = tax 0.05m 
 
     type ShoppingCartItem =
         {Item : Product;
         Quantity: int}
-        member x.Taxes = 
+        member x.BasicTaxes = 
             match x.Item.Type with
-                | ProductType.food | ProductType.book | ProductType.medical -> BasicTax(x.Item.Price)
-                | _ -> 0.0m
+                | Food | Book | Medical -> 0M
+                | _ -> basicTax x.Item.Price
+        member x.importDuty =
+            if x.Item.IsImport
+            then importDuty(x.Item.Price + x.BasicTaxes)
+            else 0M
 
-        //With Taxes = Product.Price
-
-    
-    
+                    
     module Order =
-                 
-        let PrintRecit recit = 
-            recit |> List.iter (fun x -> printf "%s" x.Item.Name)
-            recit |> List.iter (fun x -> printf "%M" x.Item.Price)
+            
+        let price (order : List<ShoppingCartItem>)  = 
+            order |> List.sumBy (fun x -> x.Item.Price * (decimal)x.Quantity) 
+
+        let taxes (order : List<ShoppingCartItem>)  = 
+            order |> List.sumBy (fun x -> (x.BasicTaxes + x.importDuty) * (decimal)x.Quantity) 
+
+        let Printorder order =             
+            order |> List.iter (fun x -> printf "%s: %M\n" x.Item.Name (System.Math.Round((x.Item.Price + x.BasicTaxes + x.importDuty),2)))
+            let totaltax = System.Math.Round((taxes order),2)
+            let total1 = (price order) + (taxes order)
+            let total = System.Math.Round(total1,2)
+            printf "Total Taxes %M\n"  totaltax
+            printf "Total %M\n"  total
+            
+
+                
+            
 
--- SalexTaxes_F/Program.fs
+++ SalexTaxes_F/Program.fs
@@ -8,17 +8,31 @@ module Program =
     [<EntryPoint>]
     let main (argv :string[]) = 
     
-        let prod1 = {Name = "book"; Type = ProductType.book; Price = 12.49m; IsImport = false; IsExempt = true} 
-        let item1 = {Item = prod1; Quantity = 1}
-        let prod2 = {Name = "music CD"; Type = ProductType.other; Price = 14.99m; IsImport = false; IsExempt = false} 
-        let item2 = {Item = prod2; Quantity = 1}
-        let prod3 = {Name = "chocolate bar"; Type = ProductType.food; Price = 0.85m; IsImport = false; IsExempt = true} 
-        let item3 = {Item = prod3; Quantity = 1}
+        let order1 =  [
+            {Item = {Name = "book"; Type = Book; Price = 12.49m; IsImport = false}; Quantity = 1};
+            {Item = {Name = "music CD"; Type = Other; Price = 14.99m; IsImport = false}; Quantity = 1};
+            {Item = {Name = "chocolate bar"; Type = Food; Price = 0.85m; IsImport = false}; Quantity = 1}
+            ]
 
-        let order =  [item1; item2; item3]
+        Order.Printorder order1
+        printf "\n"      
 
-        Order.PrintRecit order
+        let order2 =  [
+            {Item = {Name = "imported box of chocolates"; Type = Food; Price = 10.00m; IsImport = true}; Quantity = 1};
+            {Item = {Name = "imported bottle of perfume"; Type = Other; Price = 47.50m; IsImport = true}; Quantity = 1};            
+            ]
 
+        Order.Printorder order2 
+        printf "\n"          
+
+        let order3 =  [
+            {Item = {Name = "imported bottle of perfume"; Type = Other; Price = 27.99m; IsImport = true}; Quantity = 1};
+            {Item = {Name = "bottle of perfume"; Type = Other; Price = 18.99m; IsImport = false}; Quantity = 1};
+            {Item = {Name = "packet of headache pills"; Type = Medical; Price = 9.75m; IsImport = false}; Quantity = 1};
+            {Item = {Name = "box of imported chocolates"; Type = Food; Price = 11.25m; IsImport = true}; Quantity = 1}
+            ]
+
+        Order.Printorder order3
         Console.ReadLine() |> ignore
    
         0
