771b6b42dcec721079a6977d17785ceafea05bca
alexan1 <alexan1@gmail.com>
2014-05-26 10:02:20 -0400

    fsarp trafficflow

---
+++ TrafficFlow_F/Program.fs
@@ -0,0 +1,21 @@
+﻿namespace TrafficFlow
+open Traffic 
+open System   
+
+module Program =
+
+    [<EntryPoint>]
+    let main argv = 
+        
+        let distance = 150        
+        
+        printf "0) time: %d\n" (int (time distance 30  [10; 10; 10]))
+        printf "1) time: %d\n" (int (time distance 20  [10; 10; 10]))
+        printf "2) time: %d\n" (int (time distance 20  [10; 20; 30]))
+        printf "3) time: %d\n" (int (time distance 5  [10; 11; 12; 13; 14; 15]))
+        printf "4) time: %d\n" (int (time distance 5  [60; 60; 60; 60; 60; 60; 60; 60; 60; 60]))
+        printf "5) time: %d\n" (int (time distance 25  [55;29;26;12;19;39;18;20;23;28;56;20;59;48;33;40;30;60;19]))
+        
+        Console.ReadLine() |> ignore
+                
+        0 // return an integer exit code
---
+++ TrafficFlow_F/Traffic.fs
@@ -0,0 +1,22 @@
+﻿namespace TrafficFlow
+
+module Traffic = 
+
+    let isGreen currentTime switchItme =
+        let state = int currentTime / int switchItme
+        state % 2 = 0
+
+    let waitTime (currentTime : float) switchTime =  
+        match (isGreen currentTime switchTime) with
+            | false -> if (currentTime % switchTime) < 0.01 then switchTime else currentTime % switchTime
+            | _ -> 0.0
+
+            
+    let time distanceBetween speed  (lights : seq<int>) =
+        let timeBetween = float distanceBetween / float speed
+        (lights |> Seq.fold (fun acc x -> float acc + timeBetween + waitTime acc (float x) ) timeBetween)
+
+       
+
+
+
